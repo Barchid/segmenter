383d6072af5253e545c765c4dbf10eef2facc234
Sami Barchid <sami.barchid@univ-lille.fr>
2022-02-17 13:55:14 +0100

    changed in_channels as well

--- get_segmenter.py
+++ get_segmenter.py
@@ -5,9 +5,10 @@ import torch.optim as optim
 import yaml
 from timm.models.layers import trunc_normal_
 from segm.model.factory import create_segmenter
+from segm.model.vit import PatchEmbedding
 
 
-def get_segmenter(config_path: str, num_classes: int, pretrain_path: str = None):
+def get_segmenter(config_path: str, in_channels: int, num_classes: int, pretrain_path: str = None):
     config = yaml.load(
         open(config_path, "r"), Loader=yaml.FullLoader
     )
@@ -22,5 +23,9 @@ def get_segmenter(config_path: str, num_classes: int, pretrain_path: str = None)
         1, num_classes, config['net_kwargs']['d_model']))
     trunc_normal_(segmenter.decoder.cls_emb, std=0.02)
     segmenter.decoder.mask_norm = nn.LayerNorm(num_classes)
+
+    if in_channels != 3:
+        segmenter.encoder.patch_embed = PatchEmbedding(
+            config['net_kwargs']['image_size'], config['net_kwargs']['patch_size'], config['net_kwargs']['d_model'], in_channels)
 
     return segmenter
