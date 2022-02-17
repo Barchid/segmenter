8521464b4c7db8078573aeffc6368ad09b647191
Sami Barchid <sami.barchid@univ-lille.fr>
2022-02-17 14:27:05 +0100

    add datasets

---
+++ main.py
@@ -0,0 +1,104 @@
+from argparse import ArgumentParser
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import pytorch_lightning as pl
+from segmenter_module import SegmenterModule
+
+def main():
+    # seeds the random from numpy, pytorch, etc for reproductibility
+    pl.seed_everything(1234)
+
+    args = get_args()
+
+    datamodule = create_datamodule(args)
+
+    module = create_module(args, datamodule)
+
+    trainer = create_trainer(args)
+
+    # Launch training/validation
+    if args.mode == "train":
+        trainer.fit(module, datamodule=datamodule, ckpt_path=args.ckpt_path)
+
+        # report results in a txt file
+        report_path = os.path.join(args.default_root_dir, 'train_report.txt')
+        report = open(report_path, 'a')
+
+        # TODO: add any data you want to report here
+        # here, we put the model's hyperparameters and the resulting val accuracy
+        report.write(
+            f"{args.name} {args.dataset} {args.event_representation} {args.learning_rate}  {trainer.checkpoint_callback.best_model_score}\n")
+    elif args.mode == "lr_find":
+        lr_finder = trainer.tuner.lr_find(module, datamodule=datamodule)
+        fig = lr_finder.plot(suggest=True)
+        fig.show()
+        print(f'SUGGESTION IS :', lr_finder.suggestion())
+    else:
+        trainer.validate(module, datamodule=datamodule, ckpt_path=args.ckpt_path)
+
+
+def create_module(args, datamodule: SegmenterModule) -> pl.LightningModule:
+    # vars() is required to pass the arguments as parameters for the LightningModule
+    dict_args = vars(args)
+
+    # TODO: you can change the module class here
+    module = SegmenterModule(**dict_args)
+
+    return module
+
+
+def create_datamodule(args) -> pl.LightningDataModule:
+    # vars() is required to pass the arguments as parameters for the LightningDataModule
+    dict_args = vars(args)
+
+    # TODO: you can change the datamodule here
+    datamodule = DVSDataModule(**dict_args)
+
+    return datamodule
+
+
+def create_trainer(args) -> pl.Trainer:
+    # saves the best model checkpoint based on the accuracy in the validation set
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        monitor="val_acc",  # TODO: select the logged metric to monitor the checkpoint saving
+        filename="model-{epoch:03d}-{val_acc:.4f}",
+        save_top_k=1,
+        mode="max",
+    )
+
+    logger = pl.loggers.TensorBoardLogger(
+        os.path.join(args.default_root_dir, "logger"),
+        name=f"{args.name} {args.dataset} {args.event_representation}"
+    )
+
+    # create trainer
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint_callback], logger=logger)
+    return trainer
+
+
+def get_args():
+    # Program args
+    # TODO: you can add program-specific arguments here
+    parser = ArgumentParser()
+    parser.add_argument('--mode', type=str, choices=["train", "validate", "lr_find"], default="train")
+    parser.add_argument('--ckpt_path', type=str, default=None,
+                        help="Path of a checkpoint file. Defaults to None, meaning the training/testing will start from scratch.")
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--dataset', type=str, choices=["nyudv2", "sunrgbd"])
+
+    # Args for model
+    parser = SegmenterModule.add_model_specific_args(parser)
+
+    # Args for Trainer
+    parser = pl.Trainer.add_argparse_args(parser)
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    main()
---
+++ segmenter_module.py
@@ -0,0 +1,74 @@
+from argparse import ArgumentParser
+from os import times
+
+import torch
+import pytorch_lightning as pl
+from torch.nn import functional as F
+import torchmetrics
+from get_segmenter import get_segmenter
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+class SegmenterModule(pl.LightningModule):
+    def __init__(self, learning_rate: float, config_path: str, in_channels: int, num_classes: int, pretrain_path: str = None, **kwargs):
+        super().__init__()
+        self.save_hyperparameters(
+            ignore=["config_path", "pretrain_path", "in_channels", "num_classes"])
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+        self.model = get_segmenter(
+            config_path, in_channels, num_classes, pretrain_path)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = torchmetrics.functional.iou(
+            y_hat.clone().detach(), y, num_classes=self.num_classes)
+
+        self.log('train_loss', loss, on_epoch=True, prog_bar=False)
+        self.log('train_acc', acc, on_epoch=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = torchmetrics.functional.iou(
+            y_hat.clone().detach(), y, num_classes=self.num_classes)
+
+        # logs
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True)
+        self.log('val_acc', acc, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = torchmetrics.functional.iou(
+            y_hat.clone().detach(), y, num_classes=self.num_classes)
+
+        self.log('test_loss', loss, on_epoch=True, prog_bar=True)
+        self.log('test_acc', acc, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self.hparams.learning_rate)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        # Here, you add every arguments needed for your module
+        # NOTE: they must appear as arguments in the __init___() function
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('--learning_rate', type=float, default=0.0001)
+        parser.add_argument('--config_path', type=str)
+        parser.add_argument('--pretrain_path', type=str, default=None)
+        parser.add_argument('--num_classes', type=int, default=40)
+        return parser
